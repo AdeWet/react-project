a341d2d9f61bf563d23ab42005ddfb71d12181fa
AdeWet <AdeWet@jhb.dvt.co.za>
2024-08-06 13:40:39 +0200

    feat: :sparkles: added products api and relevant models

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import BrowsePage from "./components/browse/BrowsePage";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <BrowsePage />
-    </>
+    </QueryClientProvider>
   );
 }
 
--- src/components/browse/BrowsePage.tsx
+++ src/components/browse/BrowsePage.tsx
@@ -1,7 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import { getProducts } from "../../api/productsApi";
+
 const BrowsePage = () => {
+  const query = useQuery({
+    queryKey: ["products"],
+    queryFn: getProducts,
+  });
+
   return (
     <>
-      <div></div>
+      <div>
+        {query.data?.map((product) => (
+          <button>{product.title}</button>
+        ))}
+      </div>
     </>
   );
 };
