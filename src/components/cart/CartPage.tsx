c2a46f20eace49ae85e2a84196252ca13d86b860
AdeWet <AdeWet@jhb.dvt.co.za>
2024-08-07 10:12:05 +0200

    feat: :sparkles: added showing of cart items

--- src/components/cart/CartPage.tsx
+++ src/components/cart/CartPage.tsx
@@ -1,12 +1,27 @@
+import { useQuery } from "@tanstack/react-query";
+import { getProducts } from "../../api/fakestoreApi";
+import { useCartStore } from "../stores/useCartStore";
+
 const CartPage = () => {
+  const query = useQuery({
+    queryKey: ["products"],
+    queryFn: getProducts,
+  });
+  const { cart } = useCartStore();
+
   return (
     <>
       <div>
         <h2>My Cart</h2>
 
-        <div>item 1</div>
-        <div>item 1</div>
-        <div>item 1</div>
+        {cart.map((item) => (
+          <div>
+            {
+              query.data?.find((product) => product.id === item.productId)
+                ?.title
+            }
+          </div>
+        ))}
 
         <button className="btn btn-primary text-primary-content">
           Make Payment
