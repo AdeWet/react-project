4bc729825db2be2f8e283c4eae481cee348a7496
AdeWet <AdeWet@jhb.dvt.co.za>
2024-08-07 10:39:48 +0200

    feat: :sparkles: added navbar (daisyui) and navigation to browse products and to cart

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Outlet } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <Navbar />
       <Outlet />
     </QueryClientProvider>
   );
