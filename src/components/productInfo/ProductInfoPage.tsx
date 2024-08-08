c2a1d886c251411c20af16ed119a9aecf69e3f95
AdeWet <AdeWet@jhb.dvt.co.za>
2024-08-08 10:36:40 +0200

    feat: :sparkles: added adding to cart from product info page

--- src/components/productInfo/ProductInfoPage.tsx
+++ src/components/productInfo/ProductInfoPage.tsx
@@ -1,18 +1,51 @@
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 import { getProductInformation } from "../../api/fakestoreApi";
+import { priceInRands } from "../../utils";
+import { useCartStore } from "../stores/useCartStore";
 
 const ProductInfoPage = () => {
   const { productId } = useParams();
+
   const query = useQuery({
     queryKey: [productId],
     queryFn: () => getProductInformation(productId),
   });
+  const { adjustItemQuantity } = useCartStore();
+
+  if (!query.data) {
+    return;
+  }
 
   return (
-    <div>
-      <div className="card bg-base-100">{query.data?.description}</div>
-      <button className="btn btn-primary">Add to Cart</button>
+    <div className="p-4 flex flex-col">
+      <div>
+        <div className="text-xl pb-2 font-semi-bold">{query.data?.title}</div>
+        <figure className="w-full flex p-6 bg-white justify-center">
+          <img
+            className="w-4/6 object-scale-down"
+            src={query.data?.image}
+            alt="ProductInfo"
+          />
+        </figure>
+        <div className="divider"></div>
+      </div>
+      <div>
+        <div className="text-lg font-semi-bold">Description</div>
+        <div className="font-extralight">{query.data?.description}</div>
+        <div className="divider"></div>
+      </div>
+      <div className="flex flex-wrap justify-between">
+        <div className="font-bold text-2xl self-center">
+          {priceInRands(query.data?.price ?? 0)}
+        </div>
+        <button
+          className="btn btn-primary"
+          onClick={() => adjustItemQuantity(query.data.id, true)}
+        >
+          Add to Cart
+        </button>
+      </div>
     </div>
   );
 };
